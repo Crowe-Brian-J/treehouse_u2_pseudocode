8cbe1bbbc51aca7170625758d6aa15b1476e85f1
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-15 00:02:26 -0400

    refactor to deal with invalid input

--- gpa.js
+++ gpa.js
@@ -6,15 +6,13 @@ const calculateGPA = (studentGrades) => {
   // for each grade in student_grades
   for (let i = 0; i < studentGrades.length; i++) {
     // if grade is not a 1, 2, 3, or 4
-    if (studentGrades[i] < 1 && studentGrades[i] > 4) {
+    if (studentGrades[i] < 1 || studentGrades[i] > 4) {
       // print "invalid grade"
-      console.log('Invalid Grade')
       // iprint grade
-      console.log(studentGrades[i])
+      console.log('Invalid Grade: ' + studentGrades[i])
       // print "can't complete calculation"
-      console.log("Can't complete calculation.")
       // exit function
-      return
+      return "Can't complete calculation"
       // else add grade to grade_total
     } else {
       gradeTotal += studentGrades[i]
@@ -30,11 +28,11 @@ const calculateGPA = (studentGrades) => {
 }
 
 // set reggie_grades to 4, 4, 3, 4
+const reggieGrades = [4, 4, -2, 4]
 // print the results of calling calculate_gpa with reggie_grades -> 3.75
-const reggieGrades = [4, 4, 3, 4]
 console.log(calculateGPA(reggieGrades))
 
 // set dave_grades to 1, 2, 3, 2
-// print the results of calling calculate_gpa with dave_grades -> 2
 const daveGrades = [1, 2, 3, 2]
+// print the results of calling calculate_gpa with dave_grades -> 2
 console.log(calculateGPA(daveGrades))
