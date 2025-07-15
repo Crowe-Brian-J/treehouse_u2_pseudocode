0192652d200cc64d32cbf6e9a4d63e1de3f98f7b
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-14 23:24:54 -0400

    notes for pseudocode

---
+++ pseudo.js
@@ -0,0 +1,30 @@
+/*
+--- Notes for Pseudocode ---
+
+--terms/definitions
+
+* Logic - the order in which the program runs, the decisions made within the program, and how you structure the different parts of the program
+
+* Syntax - the actual programming language (i.e. "let score = 0" in js would be "int score = 0" in C#)
+
+(Should probably have done this in a .txt file instead of a js file)
+
+--what is pseudocode?
+
+* generally a set of instructions you write to yourself in something closer to plain English:
+
+print "What is your name?"
+input name
+set applicantName to input
+print "What is your GPA?"
+input gpa
+set applicantGPA to input
+if applicantGPA is less than 0 or more than 6:
+  print "Not a valid GPA, try submission again"
+  end
+elseif applicantGPA is 3 or more:
+  set applicationStatus to high
+endif
+store applicantName and applicantGPA in database
+
+*/
